353041a661b983432681ad592c9c6bc20ea5d023
oscar <oscarsmbaquero@gmail.com>
2024-08-02 20:05:34 +0200

    home cards

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -11,7 +11,10 @@ import * as AOS from 'aos';
 export class HomeComponent implements OnInit {
 
   ngOnInit() {
-    AOS.init();//AOS - 2
-    AOS.refresh();//refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
+    AOS.init({
+      duration: 1550,
+      delay: 550,
+    });//AOS - 2
+    //AOS.refresh();//refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
   }
 }
