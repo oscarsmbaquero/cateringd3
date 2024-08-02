cb1164e1ff44b949de0ebd19149df2d5b2dc79c0
oscar <oscarsmbaquero@gmail.com>
2024-08-03 00:01:35 +0200

    form init

--- src/pages/contact/contact.component.ts
+++ src/pages/contact/contact.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FloatLabelModule, FormsModule, InputTextModule, ButtonModule, InputTextareaModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  value: string | undefined;
+
+  loading: boolean = false;
+
+  load() {
+      this.loading = true;
+
+      setTimeout(() => {
+          this.loading = false
+      }, 2000);
+  }
 
 }
