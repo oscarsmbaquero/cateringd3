3397cafbf9593b30d4601473100fd81bf0eafcd8
oscar <oscarsmbaquero@gmail.com>
2024-08-03 09:51:34 +0200

    form ok

--- src/pages/contact/contact.component.ts
+++ src/pages/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { ButtonModule } from 'primeng/button';
 import { InputTextareaModule } from 'primeng/inputtextarea';
@@ -8,14 +10,29 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FloatLabelModule, FormsModule, InputTextModule, ButtonModule, InputTextareaModule],
+  imports: [CommonModule, FloatLabelModule, FormsModule, InputTextModule, ButtonModule, InputTextareaModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
   value: string | undefined;
 
+  public contact: FormGroup;
+
   loading: boolean = false;
+  public submitted: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+    
+  ) {
+    this.contact = this.formBuilder.group({
+      name: ['', [Validators.required ]],
+      mail: ['', [Validators.required]],
+      tlf: [''],
+      messagge: ['', [Validators.required]],
+    });
+  }
 
   load() {
       this.loading = true;
@@ -23,6 +40,47 @@ export class ContactComponent {
       setTimeout(() => {
           this.loading = false
       }, 2000);
+  }
+
+  public onSubmit(): void {
+    console.log('entro');
+    
+    // this.submitted = true;
+    // if (this.contact.valid) {
+      const newMessagge: any ={
+        name: this.contact.get('name')?.value,
+        mail: this.contact.get('mail')?.value,
+        tlf: this.contact.get('tlf')?.value,
+        messagge: this.contact.get('messagge')?.value,
+      }
+      console.log(newMessagge);
+      
+      // const newProduct: any = {
+      //   name: this.anadirProduct.get('name')?.value,
+      //   description: this.anadirProduct.get('description')?.value,
+      //   pCompra: parseFloat(this.anadirProduct.get('pCompra')?.value),
+      //   pvp: parseFloat(this.anadirProduct.get('pvp')?.value),
+      //   unidades: parseFloat(this.anadirProduct.get('unidades')?.value),
+      //   image: this.anadirProduct.get('image')?.value,
+      // };  
+      // this.storeService.addProduct(newProduct).subscribe(
+      //   (response: any) => {
+      //     console.log('Datos enviados con éxito');
+      //     // this.loading = false;
+      //     // this.snackBar.open(
+      //     //   'El producto ha sido añadido correctamente',
+      //     //   'Cerrar',
+      //     //   {
+      //     //     duration: 3000,
+      //     //   }
+      //     // );
+      //     this.router.navigate(['products']);
+      //   },
+      //   (error) => {
+      //     console.error('Error al enviar los datos', error);
+      //   }
+      // );
+    //}
   }
 
 }
