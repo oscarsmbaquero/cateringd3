8edabd094446229d346d1cbc6caa60ee50a553b4
oscar <oscarsmbaquero@gmail.com>
2024-08-05 13:08:19 +0200

    ajustes enlace a formulario

--- src/pages/about/about.component.ts
+++ src/pages/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  constructor(
+    private router: Router,
+    private viewportScroller: ViewportScroller
+  ){
+
+  }
+  navigateToContact() {
+    this.router.navigate(['/contact']).then(() => {
+      this.viewportScroller.scrollToPosition([0, 0]);
+    });
+  }
 
 }
